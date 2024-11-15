948b4b999ea2a75df1aec34f2521b1ef6af2fb35
Axel Munerol <axel.munerol@gmail.com>
2024-11-15 12:32:25 +0100

    tuerie

--- TestZoo.py
+++ TestZoo.py
@@ -1,15 +1,15 @@
-from Classes import Zoo,Cage,Animal
-
+from Classes import Zoo, Cage, Animal
 zoo_des_fous = Zoo()
-cont = "Yes"
 
 while True:
     action = input('Voulez-vous ajouter une [cage] ou un [animal] ? ([Quitter] pour sortir) : ').lower()
-    if action in ["cage",'zone']:
-        nom = input("nom de la cage ? ")
+
+    # Ajouter une cage
+    if action in ["cage", 'zone']:
+        nom = input("Nom de la cage ? ")
         zoo_des_fous.add_cage(Cage(nom))
 
-
+    # Ajouter un animal
     elif action in ["animal", "bebou"]:
         nom_cage = input("Nom de la cage où placer l'animal : ")
         cage_cible = zoo_des_fous.get_cage_by_name(nom_cage)
@@ -17,15 +17,20 @@ while True:
             nom_animal = input("Nom de l'animal : ")
             espece_animal = input("Espèce de l'animal : ")
             cage_cible.add_animal(Animal(nom_animal, espece_animal))
-            print(f"{nom_animal} ajouté à la cage {nom_cage}.")
         else:
             print("Cage introuvable.")
 
-
+    # Quitter
     elif action in ["quitter", "exit"]:
         break
+
+    # Saisie incorrecte
     else:
-        print('pas compris')
+        print('Pas compris')
 
+# Lancer le carnage dans chaque cage
+for cage in zoo_des_fous.cages:
+    cage.carnage()
 
+# Afficher le zoo après carnage
 print(zoo_des_fous)
